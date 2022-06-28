6f7830e78fb6c0d863b83c2c8d7b9ef4a4a9a9ba
Sergio Beltran <sergiobeltrang91@gmail.com>
2022-06-28 19:54:29 -0300

    Done

---
+++ JavaScript/Ejercicio_2/index.js
@@ -0,0 +1,10 @@
+const name = "Sergio Beltran";
+const age = 30;
+const isDeveloper = true;
+const birtDate = new Date(1991, 11, 25);
+const favoriteBook = {
+  title: "The Lord of the Rings",
+  author: "J.R.R. Tolkien",
+  year: 1954,
+  url: "https://en.wikipedia.org/wiki/The_Lord_of_the_Rings",
+};
