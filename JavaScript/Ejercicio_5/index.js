fbc8963c7b24323c7999acd77473a500e0d1a3b4
Sergio Beltran <sergiobeltrang91@gmail.com>
2022-07-04 19:53:54 -0300

    Ejercicio 5, done

---
+++ JavaScript/Ejercicio_5/index.js
@@ -0,0 +1,7 @@
+const alturaCentimentros = 170;
+const alturaMetros = 1.7;
+const pesoKilos = 57.5;
+const alturaMetrosRedondeadaUp = Math.round(alturaMetros);
+const pesoKilosRedondeadoDown = Math.floor(pesoKilos);
+const esMaximoValorEnJS = Number.MAX_VALUE + 1 === Number.MAX_VALUE;
+console.log(esMaximoValorEnJS);
