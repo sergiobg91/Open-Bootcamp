ef07afbda2eec131f0fb319c0ab49a986d82bd3d
Sergio Beltran <sergiobeltrang91@gmail.com>
2022-07-14 20:55:44 -0300

    Ejercicio3_Fechas

---
+++ JavaScript/Ejercicio_7/Ejercicio_Fechas.js
@@ -0,0 +1,6 @@
+const currentDate = new Date();
+const birthDayDate = new Date("December 25, 1991");
+const isLaterDate = currentDate > birthDayDate;
+const dayOfBirth = birthDayDate.getDate();
+const monthOfBirth = birthDayDate.getMonth();
+const yearOfBirth = birthDayDate.getFullYear();
