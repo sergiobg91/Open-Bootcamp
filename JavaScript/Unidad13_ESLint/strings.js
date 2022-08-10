c54b5202c734c76aba2cb1e703ed48cd0b8fe2f9
Sergio Beltran <sergiobeltrang91@gmail.com>
2022-08-10 19:51:34 -0300

    Unidad de práctica para eslint

---
+++ JavaScript/Unidad13_ESLint/strings.js
@@ -0,0 +1,21 @@
+var strSingleQuotes = "Hello World";
+var strDoubleQuotes = "Hello World";
+var strTemplate = "Hello World";  
+
+var myName = "Sergio";
+myName = "Sergio Beltran";
+
+console.log(strSingleQuotes);
+console.log(strDoubleQuotes);
+
+var name = "Sergio";
+var objeto= {
+    nombre: "Circulo",
+    radio: 2,
+    nombre2: "Rectangulo"
+};
+
+const prueba = 'hola'; //eslint-disable-line quotes
+
+/*eslint-disable-next-line*/
+  var prueba2 = 'prueba2'
