0da4774a3f53fecfd60d34128bd352b322f8715e
Sergio Beltran <sergiobeltrang91@gmail.com>
2022-09-16 19:11:23 -0300

    revert en cambios de package.json

---
+++ ReactJS/Básico/ob-react/src/pages/auth/LoginPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const LoginPage = () => {
+    return (
+        <form>
+            <h1>Login Page</h1>
+        </form>
+    );
+}
+
+export default LoginPage;
---
+++ ReactJS/Básico/ob-react/src/pages/task/TaskPage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const TaskPage = () => {
+  return (
+    <div>
+      <h1>Task Page</h1>
+      {/* <TaskListComponent></TaskListComponent> */}
+    </div>
+  );
+};
+
+export default TaskPage;
